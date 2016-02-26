f5c15ea08b8f7559f81a8c080755329ca3bcd3ca
Ayumu AIZAWA <ayumu.aizawa@gmail.com>
2016-02-26 11:56:33 +0900

    fix index.php

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
-
-<?
-  $ipAddress = $_SERVER["REMOTE_ADDR"];
-?>
-
 <html>
  <head>
   <title>PHP Test</title>
  </head>
  <body>
- <? echo '<p>Hello World from </p>' + $ipAddress; ?> 
+ <? echo '<p>Hello World from </p>'; ?> 
+ <? echo $_SERVER["REMOTE_ADDR"]; ?>
  </body>
 </html>
 
